270344a65dc894e292cf56824c81c1296f81a620
ahmetshbz1 <ahmetshbz1@noreply.github.com>
2025-10-07 17:49:12 +0200

    HeroUIProvider bileşenine disableRipple özelliği eklendi. Bu değişiklik, kullanıcı arayüzündeki etkileşimlerin daha akıcı hale gelmesini sağlamayı hedeflemektedir.

--- packages/admin-panel/src/main.tsx
+++ packages/admin-panel/src/main.tsx
@@ -9,7 +9,7 @@ import "./styles/global.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <HeroUIProvider>
+      <HeroUIProvider disableRipple>
         <App />
       </HeroUIProvider>
     </ThemeProvider>
